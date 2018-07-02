5097b040e0d474108c9f4d01387a22520451c7fc
Weiliang Li <to.be.impressive@gmail.com>
2018-07-02 10:37:05 +0800

    Build Python 3.7

--- setup.py
+++ setup.py
@@ -62,6 +62,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: Security :: Cryptography',
     )
