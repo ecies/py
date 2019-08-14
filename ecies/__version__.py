aef51a0c8d476a816eb5e9f02b8f6a740888f0b1
Weiliang Li <to.be.impressive@gmail.com>
2019-08-14 22:09:10 +0900

    Update __version__.py

--- ecies/__version__.py
+++ ecies/__version__.py
@@ -1,7 +1,7 @@
 __title__ = "eciespy"
 __version__ = "0.2.0"
 __description__ = "Elliptic Curve Integrated Encryption Scheme for secp256k1 in Python"
-__url__ = "https://github.com/kigawas/eciespy"
+__url__ = "https://github.com/ecies/py"
 __author__ = "Weiliang Li"
 __author_email__ = "to.be.impressive@gmail.com"
 __license__ = "MIT"
